02d628bae77ccec2a15dfd5244780a3229a31402
diyakathiria125624-design <diya.kathiria125624@marwadiuniversity.ac.in>
2026-04-04 23:30:46 +0530

    Add files via upload

---
+++ Program21/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cars c1 = new cars();
+            c1.get_data();
+            c1.get_car();
+            c1.show_vehicel();
+            c1.show_car();
+
+
+        }
+    }
+}
---
+++ Program21/vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program21
+{
+    internal class vehicle
+    {
+        string vehicle_type;
+        public void get_data()
+        {
+            Console.WriteLine("Enter Vehicel type: ");
+            vehicle_type = Console.ReadLine();
+        }
+        public void show_vehicel()
+        {
+            Console.WriteLine("vahicel Type: " + vehicle_type);
+        }
+    }
+}
