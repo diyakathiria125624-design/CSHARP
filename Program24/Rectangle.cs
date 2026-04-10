7481643b8109aee6305061a3ab0e371b48a152b7
diyakathiria125624-design <diya.kathiria125624@marwadiuniversity.ac.in>
2026-04-10 23:28:50 +0530

    Add files via upload

---
+++ Program24/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program24
+{
+    internal class Rectangle : Shape
+    {
+        double length, width;
+
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        public override double Area()
+        {
+            return length * width;
+        }
+    }
+}
---
+++ Program24/Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program24
+{
+    internal class Triangle : Shape
+    {
+        double b, h;
+
+        public Triangle(double b, double h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+
+        public override double Area()
+        {
+            return 0.5 * b * h;
+        }
+    }
+}
